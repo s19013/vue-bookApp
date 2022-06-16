78960a7009b8acab4795cb4bb7b712a3b4fcc063
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-16 19:48:04 +0900

    子コンポーネントにpropsを渡す

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     <Header/>
 
     <v-main>
-      <router-view></router-view>
+      <v-container>
+        <router-view
+        @add-book-list="addBook"
+        :books="books"
+        ></router-view>
+      </v-container>
+
     </v-main>
 
     <Footer/>
@@ -14,16 +20,53 @@
 import Header from './global/Header';
 import Footer from './global/Footer';
 
+
+const storage_key='books'
 export default {
   name: 'App',
-
   components: {
     Header,
     Footer,
   },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      books:[],
+      newBooks:null
+    }
+  },
+  methods: {
+    addBook(e){//eで子供コンポーネントの値を受け取れる
+    if (!e) { return }
+      this.books.push({
+        id:this.books.length,
+        title:e.title,
+        image:e.image,
+        description:e.description,
+        readDate:'',
+        memo:'',
+      })
+      this.saveBook()//セーブ
+      //最後に追加したidのページに移動
+      this.goToEditPage(this.books.slice(-1)[0].id)
+    },
+    removeBook(arg){
+      this.books.splice(arg,1)//削除
+      this.saveBook()//セーブ
+    },
+    saveBook(){
+      const parsed= JSON.stringify(this.books);
+      localStorage.setItem(storage_key,parsed)
+    },
+    goToEditPage(id){
+      this.$router.push(`/edit/${id}`)
+    }
+  },
+  mounted(){
+    // 初期起動ときローカルストレージがなければ作る
+    if (localStorage.getItem(storage_key)) {
+      try { this.books = JSON.parse(localStorage.getItem(storage_key)) } 
+      catch (e) { localStorage.removeItem(storage_key) }
+    }
+  }
 };
 </script>
