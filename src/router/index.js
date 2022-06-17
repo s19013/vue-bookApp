d35f15ab6b1c0ff0d27110f6ad438c2e997cd5cd
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-17 17:14:43 +0900

    再構築

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+// import Router from 'vue-router'
 import BookIndex from '../views/BookIndex.vue'
 import BookSearch from '../views/BookSearch.vue'
 import BookEdit from '../views/BookEdit.vue'
+import VueRouter from 'vue-router'
 
 
-Vue.use(Router)
+// Vue.use(Router)
 
-export default new Router({
-    mode:'history',
-    routes:[
+// export default new Router({
+//     mode:'history',
+//     routes:[
+//         {
+//           path: '/',
+//           name: 'BookIndex',
+//           component: BookIndex
+//         },
+//         {
+//           path: '/search',
+//           name: 'BookSearch',
+//           component: BookSearch
+//         },
+//         {
+//           path: '/edit/:id',
+//           name: 'BookEdit',
+//           component: BookEdit
+//         },
+//       ]
+// })
+
+Vue.use(VueRouter)
+
+const routes = [
         {
           path: '/',
           name: 'BookIndex',
@@ -24,6 +46,13 @@ export default new Router({
           path: '/edit/:id',
           name: 'BookEdit',
           component: BookEdit
-        },
-      ]
+        }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+
+export default router
